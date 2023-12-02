72361d916d64df3ff81c1ffe137743bc964ff4fd
fstermann <fabian.stermann@gmail.com>
2023-12-02 11:01:09 +0100

    feat: add day02

---
+++ day02/part2.py
@@ -0,0 +1,65 @@
+import os
+from dataclasses import dataclass
+
+import pytest
+
+from aoc.utils import read_input
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), "input.txt")
+
+
+@dataclass
+class CubeSet:
+    red: int = 0
+    green: int = 0
+    blue: int = 0
+
+    def mult(self) -> int:
+        return self.red * self.green * self.blue
+
+
+@dataclass
+class Game:
+    idx: int
+    cubesets: list[CubeSet]
+
+    def min_cubes(self) -> CubeSet:
+        return CubeSet(
+            max(cube_set.red for cube_set in self.cubesets),
+            max(cube_set.green for cube_set in self.cubesets),
+            max(cube_set.blue for cube_set in self.cubesets),
+        )
+
+
+def parse_game(line: str) -> Game:
+    line = line.replace("Game ", "")
+    idx, cubes = line.split(": ")
+    cube_sets = cubes.split("; ")
+    cube_sets = [[c.split() for c in cs.split(", ")] for cs in cube_sets]
+    cubes = [CubeSet(**{color: int(num) for num, color in cs}) for cs in cube_sets]
+    return Game(int(idx), cubes)
+
+
+def run(input_: str) -> int:
+    games = [parse_game(line) for line in input_.splitlines()]
+    return sum(game.min_cubes().mult() for game in games)
+
+
+INPUT = """\
+Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green  
+"""
+EXPECTED = 2286
+
+
+@pytest.mark.parametrize(("input_", "expected"), ((INPUT, EXPECTED),))
+def test_puzzle(input_, expected):
+    assert run(input_) == expected
+
+
+if __name__ == "__main__":
+    input_ = read_input(INPUT_TXT)
+    print(run(input_))
